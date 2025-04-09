db55877c4887e100fefb8ff63d24273bf0126bce
Yandriel90 <yandriel900724@gmail.com>
2025-04-09 16:44:40 +0200

    Add HomeComponent and update routing to include home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 
 @NgModule({
+  declarations: [],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HomeComponent,
     AppComponent, 
     ProductComponent 
   ],
   providers: [],
-  bootstrap: []
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
-import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
-export const routes: Routes = [
-  { path: '', component: AppComponent },
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, 
   { path: 'products', component: ProductComponent },
-  ];
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
